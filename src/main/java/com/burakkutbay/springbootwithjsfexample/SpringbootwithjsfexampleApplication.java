0318802224187db251650a5e2b29f656b0ea1dc0
BrkSe <brkktby@gmail.com>
2020-06-10 10:22:19 +0300

    Initial commit

---
+++ src/main/java/com/burakkutbay/springbootwithjsfexample/SpringbootwithjsfexampleApplication.java
@@ -0,0 +1,13 @@
+package com.burakkutbay.springbootwithjsfexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootwithjsfexampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootwithjsfexampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/burakkutbay/springbootwithjsfexample/SpringbootwithjsfexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.burakkutbay.springbootwithjsfexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootwithjsfexampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
